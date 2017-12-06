69522e56bbb94d50b239a29bc5d17e726c94ccd7
GabeEscalaw <33886258+GabeEscalaw@users.noreply.github.com>
2017-12-06 23:21:49 +0800

    Update and rename ValuesEdit.java to Values.java

--- Values.java
+++ Values.java
@@ -1,7 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-public class ValuesEdit{
+/**
+* <h1> Values </h1>
+*
+* Java class that contains all the value strings of the database
+* @author Mico Aquino, Elise Gabriel Escalaw, John Eugene Tiongco
+* @1.0
+* @since November 28, 2017
+*/
+
+public class Values{
 	
 	private final int START_POINTER = 0;
 	private final int STRING_MAX_LENGTH = 256;
@@ -10,8 +19,17 @@ public class ValuesEdit{
 	private final int HEADER_SIZE = 8;
 	private RandomAccessFile data;
 	private long numRecords;
-
-	public ValuesEdit(String fileName) throws IOException{ 
+	
+	/**
+	* Constructor for Values which initializes the actual file 
+	* Checks the initial file's byte and writes the number of existing records
+	* 
+	* @param fileName	name of the file
+	* @throws IOException
+	* @see IOException
+	*/
+	
+	public Values(String fileName) throws IOException{ 
 		File file = new File(fileName); //makes the actual file for the values
 		if(!file.exists()){  //checks the initial byte
 			this.numRecords = 0;
@@ -25,6 +43,13 @@ public class ValuesEdit{
 			this.numRecords = this.data.readLong(); //reads the initial bytes 
 		}
 	}
+	
+	/**
+	* insertEntry seeks the initial pointer and increments the number of records 
+	* it also writes the length of the entry into the file by accessing getBytes
+	* 
+	* @param entry	byte that will be read
+	*/	
 
 	public void insertEntry(String entry) throws IOException{
 		this.data.seek(START_POINTER);
@@ -36,6 +61,13 @@ public class ValuesEdit{
 		this.data.write(word);
 	}
 
+	/**
+	* updateEntry writes the updated length and word for the output file
+	* 
+	* @param point	pointer for locating the the characters that were encoded into 8 bytes (UTF8)
+	* @param entry	byte that will be read
+	*/
+	
 	public void updateEntry(int point, String entry) throws IOException{
 		byte[] word = entry.getBytes("UTF8");
 		this.data.seek(HEADER_SIZE+point*(TOTAL_BYTE_SIZE));
@@ -43,6 +75,12 @@ public class ValuesEdit{
 		this.data.write(word); //writes the byte[] word to the values file
 	}
 
+	/**
+	* readEntry reads the word that is being pointed to 
+	* 
+	* @param point	pointer for locating the the characters that were encoded into 8 bytes (UTF8)
+	*/
+	
 	public String readEntry(int point) throws IOException{
 		this.data.seek(HEADER_SIZE+point*(TOTAL_BYTE_SIZE)); 
 		int size = this.data.readShort();
